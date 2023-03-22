88f170f44b3a39c80125a2cc032fee5243ee05fd
Amogh Arya <amogharya2@gmail.com>
2023-03-22 23:53:36 +0530

    change

--- hello.py
+++ hello.py
@@ -8,4 +8,24 @@ class Query(graphene.ObjectType):
 
 schema = graphene.Schema(query=Query)
 result = schema.execute('{ hello }')
+print(result.data['hello']) # "Hello World"import graphene
+
+class Query(graphene.ObjectType):
+  hello = graphene.String(name=graphene.String(default_value="world"))
+
+  def resolve_hello(self, info, name):
+    return 'Hello ' + name
+
+schema = graphene.Schema(query=Query)
+result = schema.execute('{ hello }')
+print(result.data['hello']) # "Hello World"import graphene
+
+class Query(graphene.ObjectType):
+  hello = graphene.String(name=graphene.String(default_value="world"))
+
+  def resolve_hello(self, info, name):
+    return 'Hello ' + name
+
+schema = graphene.Schema(query=Query)
+result = schema.execute('{ hello }')
 print(result.data['hello']) # "Hello World"
