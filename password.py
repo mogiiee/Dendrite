d8e15ddabd6a1185e6a08c5cd774b3d4cd9fff64
Amogh Arya <amogharya2@gmail.com>
2023-03-26 23:24:45 +0530

    update

--- password.py
+++ password.py
@@ -1,8 +1,12 @@
 import random
 import string
 
-def gen_pass():
+def gen_pass(length):
     chars = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(chars) for _ in range(length))
     return password
+
+
+password = gen_pass(234)
+print(password)
 
