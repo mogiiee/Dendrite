e1a3e51dc15a5bb1404153765bac2b99ceb66a3e
Amogh Arya <amogharya2@gmail.com>
2023-03-26 23:22:52 +0530

    CHANGE:pass

--- password.py
+++ password.py
@@ -3,3 +3,6 @@ import string
 
 def gen_pass():
     chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+    return password
+
